00f24f30c3b684f7652fe2faa7c3db2c9ea3d934
Javier Bueno <javier17he@gmail.com>
2024-03-06 13:49:39 -0300

    v.30

--- AppCat/urls.py
+++ AppCat/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path("nuevo_cliente", agregar_Cliente),
     path("nueva_compra",agregar_compra),
     path("crear_presupuesto",agregar_presupuesto),
-    path("buscar_cliente", buscar_cliente)
-    path("resultadoClu",resultadoBusqueda),
+    path("buscar_cliente", buscar_cliente),
+    path("resultadoCli",resultado_busqueda),
     path("",inicio, name="Home"),
  ]
