66fdf21a1363e173cd86e6214ebe5428d2697aba
Javier Bueno <javier17he@gmail.com>
2024-03-18 11:19:47 -0300

    v.15

--- AppCat/forms.py
+++ AppCat/forms.py
@@ -1,12 +1,11 @@
 from django import forms
-
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 class ClienteForm(forms.Form):
     nombre = forms.CharField(max_length=50)
     edad = forms.IntegerField()
     direccion = forms.CharField (max_length=100)
     
-
-
 class CompraForm(forms.Form):
     fecha = forms.DateField()
     productos =forms.CharField()
@@ -14,7 +13,6 @@ class CompraForm(forms.Form):
     nombreFiesta = forms.CharField(max_length=50)
     cliente = forms.CharField(max_length=50)
 
-    
 class presupuestoForm(forms.Form):
     nombre = forms.CharField(max_length=50)
     precio = forms.IntegerField()
@@ -22,3 +20,13 @@ class presupuestoForm(forms.Form):
     direccion = forms.CharField(max_length=100)
     cliente = forms.CharField(max_length=50)
 
+class EditarClienteForm(UserChangeForm):
+
+    password = None
+
+    class Meta:
+        model = User
+        fields = ["first_name", "last_name",  "email"]
+
+class AvatarFormulario(forms.Form):
+    imagen = forms.ImageField()
--- AppCat/models.py
+++ AppCat/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Cliente(models.Model):
     nombre = models.CharField(max_length=50)
@@ -29,3 +31,6 @@ class presupuesto(models.Model):
     def __str__(self) :
         return self.nombre
     
+class Avatar(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    imagen = models.ImageField(upload_to="avatares", null=True, blank=True)
