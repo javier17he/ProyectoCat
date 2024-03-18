553dd117b598e10bb6a600dfaa468e33ac270eb2
Javier Bueno <javier17he@gmail.com>
2024-03-18 11:59:44 -0300

    v4.0

--- AppCat/views.py
+++ AppCat/views.py
@@ -13,6 +13,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 def inicio(request):
     return render(request,"AppCat/inicio.html")
 
+@login_required
 def agregar_Cliente(request):
     if request.method =="POST":
         formulario = ClienteForm(request.POST)
@@ -25,8 +26,8 @@ def agregar_Cliente(request):
         formulario = ClienteForm()
     
     return render(request, "AppCat/nuevo_cliente.html",{"form":formulario})
-    
 
+@login_required
 def agregar_presupuesto(request):
     if request.method =="POST":
         formulario = presupuestoForm(request.POST)
@@ -40,6 +41,7 @@ def agregar_presupuesto(request):
     
     return render(request, "AppCat/crear_presupuesto.html",{"form":formulario})
 
+@login_required
 def agregar_compra(request):
     if request.method =="POST":
         formulario = CompraForm(request.POST)
@@ -52,7 +54,6 @@ def agregar_compra(request):
         formulario = CompraForm()
     
     return render(request, "AppCat/nueva_compra.html",{"form":formulario})
-
 
 def buscar_cliente(request):
 
@@ -156,246 +157,93 @@ def cerrar_sesion(request):
 
     return render(request, "inicio.html", {"mensaje":"Has cerrado sesión con éxito."})
 
-def crear_estudiante(request):
 
-    est_1 = Estudiantes(nombre="Diego", apellido="De La Fuente", email="estudiante@ch.com", edad=28)
+def ver_presupuesto(request):
 
-    est_2 = Estudiantes(nombre="Belen", apellido="Ulloa", email="estudiante2@ch.com", edad=38)
+    todos_presupuesto = presupuesto.objects.all() #obtener todos los estudiantes que existen
 
-    est_1.save()
-    est_2.save()
+    return render(request, "ver_presupuesto.html", {"total":todos_presupuesto})
 
-    info = {"nombre1":est_1.nombre, "nombre2":est_2.nombre}
-
-
-    return render(request, "estudiantes/crear_estudiantes.html", info) #1er arg -- request, 2do arg -- template, 3er arg -- contexto(diccionario)
-
-#Con formulario (crear con valores especificos)
 @login_required
-def crear_nuevo_estudiante(request):
+def actualizar_presupuesto(request, presupuesto):
+
+    presupuesto_elegido = presupuesto.objects.get(id=presupuesto)
 
     if request.method == "POST":
 
-        formulario = EstudianteFormulario(request.POST,request.FILES) #almacena la informacion que se ha puesto en el form
-
-        if formulario.is_valid():
-
-            info_dic = formulario.cleaned_data #convierte la info del form a un diccionario de python
-
-            estudiante_nuevo = Estudiantes(
-                nombre=info_dic["nombre"],
-                apellido=info_dic["apellido"],
-                email=info_dic["email"],
-                edad=info_dic["edad"],
-                profesor=info_dic["profesor"],
-                imagen=info_dic["imagen"]
-                )
-            
-            estudiante_nuevo.save()
-
-            return render(request, "inicio.html")
-        
-    else:
-
-        formulario = EstudianteFormulario()
-
-    return render(request, "estudiantes/crear_estudiantes.html", {"formu":formulario})
-
-
-def ver_estudiantes(request):
-
-    todos_estudiantes = Estudiantes.objects.all() #obtener todos los estudiantes que existen
-
-    return render(request, "estudiantes/ver_estudiantes.html", {"total":todos_estudiantes})
-
-@login_required
-def actualizar_estudiantes(request, estudiante_info):
-
-    estudiante_elegido = Estudiantes.objects.get(id=estudiante_info)
-
-    if request.method == "POST":
-
-        formulario = EstudianteFormulario(request.POST) #almacena la informacion que se ha puesto en el form
+        formulario = presupuestoForm(request.POST) #almacena la informacion que se ha puesto en el form
 
         if formulario.is_valid():
 
             info_dic = formulario.cleaned_data #convierte la info del form a un diccionario de python
 
             #Cambiado los datos de un objeto que ya se habia creado!!
-            estudiante_elegido.nombre = info_dic["nombre"]
-            estudiante_elegido.apellido = info_dic["apellido"]
-            estudiante_elegido.email = info_dic["email"]
-            estudiante_elegido.edad = info_dic["edad"]
+            presupuesto_elegido.nombre = info_dic["nombre"]
+            presupuesto_elegido.precio = info_dic["precio"]
+            presupuesto_elegido.direccion = info_dic["direccion"]
+            presupuesto_elegido.producto = info_dic["producto"]
+            presupuesto_elegido.cliente = info_dic["cliente"]
 
-            estudiante_elegido.save()
+
+            presupuesto_elegido.save()
 
             return render(request, "inicio.html")
         
     else:
 
-        formulario = EstudianteFormulario(initial={"nombre":estudiante_elegido.nombre, "apellido":estudiante_elegido.apellido,"email":estudiante_elegido.email, "edad":estudiante_elegido.edad})
+        formulario = presupuestoForm(initial={"nombre":presupuesto_elegido.nombre, "precio":presupuesto_elegido.precio,"direccion":presupuesto_elegido.direccion, "producto":presupuesto_elegido.producto,"cliente":presupuesto_elegido.cliente })
 
-    return render(request, "estudiantes/actualizar_estudiantes.html", {"formu":formulario})
+    return render(request, "estudiantes/actualizar_presupuesto.html", {"formu":formulario})
 
 @login_required
-def borrar_estudiante(request, estudiante_info):
+def borrar_presupuesto(request, presupuesto_info):
 
-    estudiante_elegido = Estudiantes.objects.get(id=estudiante_info)
+    presupuesto_elegido = presupuesto.objects.get(id=presupuesto_info)
 
-    estudiante_elegido.delete()
+    presupuesto.delete()
 
     return render(request, "inicio.html")
 
-# CRUD de Cursos
-
-def crear_curso(request):
-
-    if request.method == "POST": #Cuando aprieto el boton de enviar!!!
-        
-        curso_nuevo = Curso(nombre=request.POST["nombre"],comision=request.POST["comision"])
-        #Leer la informacion y guardarla en la base de datos!!!
-        curso_nuevo.save()
-
-        return render(request, "inicio.html")
-
-    
-    return render(request, "cursos/crear_cursos.html")
-
 @login_required
-def ver_cursos(request):
+def ver_compra(request):
 
-    todos_cursos = Curso.objects.all()
+    todos_compra = Compra.objects.all()
 
-    return render(request, "cursos/ver_cursos.html", {"total":todos_cursos})
+    return render(request, "cursos/ver_compra.html", {"total":todos_compra})
 
+def borrar_presupuesto(request, presupuesto_info):
 
+    presupuesto_elegido = Compra.objects.get(id=presupuesto_info)
 
-def borrar_curso(request, curso_info):
-
-    curso_elegido = Curso.objects.get(id=curso_info)
-
-    curso_elegido.delete()
+    presupuesto_elegido.delete()
 
     return render(request, "inicio.html")
 
+def ver_cliente(request):
 
-# CRUD de Profesores
+    return render(request, "ver_client.html")
 
-def crear_profesor(request):
+class CompraLista(ListView):
+    model = Compra
+    template_name = "lista_de_compras.html"
 
-    if request.method == "POST":
+class CompraCrear(LoginRequiredMixin, CreateView):
+    model = Compra
+    template_name = "nueva_compra.html"
+    fields = ["nombre_fiesta", "cliente"]
+    success_url = "/AppCat/ver_compra/"
 
-        formulario = ProfesorFormulario(request.POST) #almacena la informacion que se ha puesto en el form
-
-        if formulario.is_valid():
-
-            info_dic = formulario.cleaned_data #convierte la info del form a un diccionario de python
-
-            profesor_nuevo = Profesor(
-                nombre=info_dic["nombre"],
-                apellido=info_dic["apellido"],
-                email=info_dic["email"],
-                profesion=info_dic["profesion"]
-                )
-            
-            profesor_nuevo.save()
-
-            return render(request, "inicio.html")
-        
-    else:
-
-        formulario = ProfesorFormulario()
-
-    return render(request, "profes/crear_profesor.html", {"formu":formulario})
-
-def ver_profesores(request):
-
-    return render(request, "profes/ver_profes.html")
-
-
-#CRUD de Entregables
-
-#Crear un entregable en la base de datos usando un formulario    
-def crear_entregable(request):
-
-    if request.method == "POST":
-
-        formulario = EntregableFormulario(request.POST) #almacena la informacion que se ha puesto en el form
-
-        if formulario.is_valid():
-
-            info_dic = formulario.cleaned_data #convierte la info del form a un diccionario de python
-
-            entrega_nueva = Entregable(
-                nombre=info_dic["nombre"],
-                fechaEntrega=info_dic["fechaEntrega"],
-                entregado=info_dic["entregado"]
-                )
-            
-            entrega_nueva.save()
-
-            return render(request, "inicio.html")
-        
-    else:
-
-        formulario = EntregableFormulario()
-
-    return render(request, "entregas/crear_entregable.html", {"formu":formulario})
-
-def ver_entregables(request):
-
-    return render(request, "entregas/ver_entregables.html")
-
-
-# Busqueda 
-
-def buscar_curso(request):
-    
-
-    if request.GET: #Solo si es que hay una busqueda!!!
-
-        nombre = request.GET["nombre"]  #leer el diccionario de info del formulario y obtengo el valor de busqueda
-        cursos = Curso.objects.filter(nombre__icontains=nombre) #filtrar todos los cursos que tengan dicho nombre!!
-        
-        mensaje = f"Estamos buscando al curso {nombre}"
-
-        return render(request, "cursos/resultados_cursos.html", {"mensaje":mensaje, "resultados":cursos})
-
-    
-    return render(request, "cursos/resultados_cursos.html") #si todavia no hay una busqueda
-
-
-# Vistas basadas en Clases!!
-
-class CursosLista(ListView):
-    model = Curso
-    template_name = "cursos/lista_de_cursos.html"
-
-# Create del CRUD
-class CursosCrear(LoginRequiredMixin, CreateView):
-    model = Curso
-    template_name = "cursos/nuevo_curso.html"
-    fields = ["nombre", "comision"]
-    success_url = "/AppCoder/ver_cursos/"
-    
-
-# Update del CRUD
-    
-class CursosEditar(UpdateView):
-    model = Curso
-    template_name = "cursos/nuevo_curso.html"
-    fields = ["nombre", "comision"]
+class PresupuestoEditar(UpdateView):
+    model = presupuesto
+    template_name = "crear_presupuesto.html"
+    fields = ["nombre", "cliente"]
     success_url = "/AppCoder/"
 
-
-# Delete del CRUD
-
-class CursosBorrar(DeleteView):
-    model = Curso
-    template_name = "cursos/borrar_curso.html"
-    success_url = "/AppCoder/"
-
+class presupuestoBorrar(DeleteView):
+    model = presupuesto
+    template_name = "borrar_presupuesto.html"
+    success_url = "/AppCat/"
 
 class CambiarContra(LoginRequiredMixin, PasswordChangeView):
-    template_name = "registro/cambiar_contra.html"
-    success_url = "/AppCoder/"
+    template_name = "cambiar_contra.html"
+    success_url = "/AppCat/"
